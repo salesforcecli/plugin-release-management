2b899194cea35f399e11785ccf1d538dcbea7a72
Benjamin Maggi <bmaggi@salesforce.com>
2021-04-05 15:00:55 -0300

    fix: handle lerna repo

--- src/commands/typescript/update.ts
+++ src/commands/typescript/update.ts
@@ -9,10 +9,10 @@ import * as path from 'path';
 import * as glob from 'glob';
 import { flags, FlagsConfig, SfdxCommand } from '@salesforce/command';
 import { fs, Messages, SfdxError } from '@salesforce/core';
-import { exec, pwd, cd } from 'shelljs';
+import { exec, pwd } from 'shelljs';
 import { set } from '@salesforce/kit';
 import { AnyJson, asObject, getString } from '@salesforce/ts-types';
-import { NpmPackage, Package } from '../../package';
+import { NpmPackage, Package, PackageJson } from '../../package';
 import { SinglePackageRepo, LernaRepo, LernaJson, isMonoRepo } from '../../repository';
 
 Messages.importMessagesDirectory(__dirname);
@@ -33,49 +33,38 @@ export default class Update extends SfdxCommand {
     }),
   };
 
-  private typescriptPkgs: NpmPackage[];
+  private typescriptPkg: NpmPackage;
+  private repo: SinglePackageRepo | LernaRepo;
+  private packages: Package[];
 
   public async run(): Promise<void> {
-    this.typescriptPkgs = await this.retrieveTsPackages();
+    this.typescriptPkg = this.retrieveTsPackage();
     this.validateEsTarget();
     this.validateTsVersion();
 
-    const pkg = (await isMonoRepo())
+    this.repo = (await isMonoRepo())
       ? await LernaRepo.create({ ux: this.ux })
       : await SinglePackageRepo.create({ ux: this.ux });
+
+    this.packages = this.getPackages();
 
     this.ux.warn('This is for testing new versions only. To update the version you must go through dev-scripts.');
 
     await this.updateTsVersion();
     await this.updateEsTarget();
 
-    if (await isMonoRepo()) {
-      const packagePaths = await this.getPackagePaths();
-      const workingDir = pwd().stdout;
-
-      for (const packagePath of packagePaths) {
-        cd(path.resolve(packagePath));
-        try {
-          pkg.install();
-          pkg.build();
-          pkg.test();
-        } finally {
-          this.ux.log('Reverting unstaged stages');
-          cd(workingDir);
-          pkg.revertUnstagedChanges();
-        }
-        cd(workingDir);
-      }
-    } else {
-      try {
-        pkg.install();
-        pkg.build();
-        pkg.test();
-      } finally {
-        this.ux.log('Reverting unstaged stages');
-        pkg.revertUnstagedChanges();
-      }
+    try {
+      this.repo.install();
+      this.repo.build();
+      this.repo.test();
+    } finally {
+      this.ux.log('Reverting unstaged stages');
+      this.repo.revertUnstagedChanges();
     }
+  }
+
+  private getPackages(): Package[] {
+    return this.repo instanceof LernaRepo ? this.repo.packages : [this.repo.package];
   }
 
   private async getPackagePaths(): Promise<string[]> {
@@ -114,71 +103,48 @@ export default class Update extends SfdxCommand {
     return;
   }
 
-  private async updatePackage(packagePath: string, npmPackage: NpmPackage): Promise<void> {
-    const newVersion = this.determineNextTsVersion(npmPackage);
-    const pkg = await Package.create(path.resolve(packagePath));
-    if (pkg.packageJson.devDependencies['typescript']) {
-      this.ux.log(`Updating typescript version to ${newVersion}`);
-      pkg.packageJson.devDependencies['typescript'] = newVersion;
-      pkg.packageJson.devDependencies['@typescript-eslint/eslint-plugin'] = 'latest';
-      pkg.packageJson.devDependencies['@typescript-eslint/parser'] = 'latest';
-    }
-    // If the prepare script runs sf-install, the install will fail because the typescript version
-    // won't match the expected version. So in that case, we delete the prepare script so that we
-    // get a successful install
-    if (pkg.packageJson.scripts['prepare'] === 'sf-install') {
-      delete pkg.packageJson.scripts['prepare'];
-    }
-    return pkg.writePackageJson();
-  }
-
   private async updateTsVersion(): Promise<void> {
-    if (await isMonoRepo()) {
-      const packagePaths = await this.getPackagePaths();
-      const runner = exec('npm view typescript --json', { silent: true });
-      if (runner.code !== 0) {
-        throw new SfdxError('Could not find typescript on the npm registry', 'TypescriptNotFound');
+    const newVersion = this.determineNextTsVersion();
+    for (const pkg of this.packages) {
+      if (pkg.packageJson.devDependencies['typescript']) {
+        this.ux.log(`Updating typescript version to ${newVersion}`);
+        pkg.packageJson.devDependencies['typescript'] = newVersion;
+        pkg.packageJson.devDependencies['@typescript-eslint/eslint-plugin'] = 'latest';
+        pkg.packageJson.devDependencies['@typescript-eslint/parser'] = 'latest';
       }
-      for (const packagePath of packagePaths) {
-        const npmPackage = JSON.parse(runner.stdout) as NpmPackage;
-        await this.updatePackage(packagePath, npmPackage);
+
+      // If the prepare script runs sf-install, the install will fail because the typescript version
+      // won't match the expected version. So in that case, we delete the prepare script so that we
+      // get a successful install
+      if (pkg.packageJson.scripts['prepare'] === 'sf-install') {
+        delete pkg.packageJson.scripts['prepare'];
       }
-      return;
+
+      pkg.writePackageJson(pkg.location);
     }
 
-    this.typescriptPkgs.map(async (typescriptPkg) => {
-      this.ux.log(typescriptPkg.version);
-      await this.updatePackage(path.resolve('.'), typescriptPkg);
-    });
+    if (this.repo instanceof LernaRepo) {
+      const pkgJson = (await fs.readJson('package.json')) as PackageJson;
+      // If the install script runs sf-lerna-install, the install will fail because the typescript version
+      // won't match the expected version. So in that case, we delete the prepare script so that we
+      // get a successful install
+      if (pkgJson.scripts['install'] === 'sf-lerna-install') {
+        delete pkgJson.scripts['install'];
+        await fs.writeJson('package.json', pkgJson);
+      }
+    }
   }
 
-  private determineNextTsVersion(typescriptPkg: NpmPackage): string {
+  private determineNextTsVersion(): string {
     return this.flags.version === 'latest' || !this.flags.version
-      ? getString(typescriptPkg, 'dist-tags.latest')
+      ? getString(this.typescriptPkg, 'dist-tags.latest')
       : (this.flags.version as string);
   }
 
-  private async retrieveTsPackages(): Promise<NpmPackage[]> {
-    // Process lerna repos
-    if (await isMonoRepo()) {
-      const workingDir = pwd().stdout;
-      const lernaProjects = await this.getPackagePaths();
-      const result = lernaProjects.map((packagePath) => {
-        cd(path.resolve(packagePath));
-        const runner = exec('npm view typescript --json', { silent: true });
-        cd(workingDir);
-        if (runner.code === 0) {
-          return JSON.parse(runner.stdout) as NpmPackage;
-        } else {
-          throw new SfdxError('Could not find typescript on the npm registry', 'TypescriptNotFound');
-        }
-      });
-      return result;
-    }
-    // Process regular packages
-    const runner = exec('npm view typescript --json', { silent: true });
-    if (runner.code === 0) {
-      return [JSON.parse(runner.stdout)] as NpmPackage[];
+  private retrieveTsPackage(): NpmPackage {
+    const result = exec('npm view typescript --json', { silent: true });
+    if (result.code === 0) {
+      return JSON.parse(result.stdout) as NpmPackage;
     } else {
       throw new SfdxError('Could not find typescript on the npm registry', 'TypescriptNotFound');
     }
@@ -196,10 +162,7 @@ export default class Update extends SfdxCommand {
 
   private validateTsVersion(): boolean {
     if (this.flags.version === 'latest') return true;
-    if (
-      this.flags.version &&
-      !this.typescriptPkgs.some((typescriptPkg) => typescriptPkg.versions.includes(this.flags.version))
-    ) {
+    if (this.flags.version && !this.typescriptPkg.versions.includes(this.flags.version)) {
       throw SfdxError.create('@salesforce/plugin-release-management', 'typescript.update', 'InvalidTypescriptVersion', [
         this.flags.version,
       ]);
