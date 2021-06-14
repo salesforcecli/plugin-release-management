023105abf7aea2391db78e8d7a8f7e88f74412e7
mshanemc <shane.mclaughlin@salesforce.com>
2021-06-14 13:53:31 -0500

    refactor: prune dead code

--- src/codeSigning/upload.ts
+++ src/codeSigning/upload.ts
@@ -5,8 +5,6 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import { basename } from 'path';
-import { fs } from '@salesforce/core';
 import * as AWS from 'aws-sdk';
 
 export async function putObject(bucket: string, key: string, body: string): Promise<AWS.S3.PutObjectOutput> {
@@ -17,28 +15,4 @@ export async function putObject(bucket: string, key: string, body: string): Prom
       if (resp) resolve(resp);
     });
   });
-}
-
-function getUploadKey(signaturePath: string, keyPrefix?: string): string {
-  const sigFileName = basename(signaturePath);
-  let uploadKey: string;
-  if (keyPrefix && keyPrefix.endsWith('/')) {
-    uploadKey = `${keyPrefix}${sigFileName}`;
-  } else if (keyPrefix) {
-    uploadKey = `${keyPrefix}/${sigFileName}`;
-  } else {
-    uploadKey = sigFileName;
-  }
-  return uploadKey;
-}
-
-export async function upload(
-  signaturePath: string,
-  bucket: string,
-  keyPrefix?: string
-): Promise<AWS.S3.PutObjectOutput> {
-  const signature = await fs.readFile(signaturePath, { encoding: 'utf-8' });
-  const sigFileName = basename(signaturePath);
-  const uploadKey = getUploadKey(sigFileName, keyPrefix);
-  return putObject(bucket, uploadKey, signature);
 }
