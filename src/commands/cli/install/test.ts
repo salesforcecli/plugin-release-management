4b5aeabdfa5b96376705a50cc1f22c5929a686c3
Mike Donnalley <mdonnalley@salesforce.com>
2021-10-22 13:04:11 -0600

    fix: make executables OS safe

--- src/commands/cli/install/test.ts
+++ src/commands/cli/install/test.ts
@@ -31,6 +31,11 @@ enum Channel {
 export type Results = Record<string, Record<CLI, boolean>>;
 export type ServiceAvailability = { service: string; available: boolean };
 
+function makeOsSafe(executable: string): string {
+  if (process.platform === 'win32') return `& "${executable}"`;
+  else return executable;
+}
+
 namespace Method {
   export enum Type {
     INSTALLER = 'installer',
@@ -238,7 +243,7 @@ class Tarball extends Method.Base {
     for (const cli of this.getTargets()) {
       const executable = path.join(directory, 'bin', process.platform === 'win32' ? `${cli}.cmd` : cli);
       this.logger.log(`Testing ${chalk.cyan(executable)}`);
-      const result = exec(`${executable} --version`, { silent: true });
+      const result = exec(`${makeOsSafe(executable)} --version`, { silent: true });
       this.logger.log(chalk.dim((result.stdout ?? result.stderr).replace(/\n*$/, '')));
       results[cli] = result.code === 0;
     }
@@ -318,7 +323,7 @@ class Npm extends Method.Base {
           ? which(CLI.SF).stdout
           : path.join(this.options.directory, 'node_modules', '.bin', cli);
       this.logger.log(`Testing ${chalk.cyan(executable)}`);
-      const result = exec(`${executable} --version`, { silent: true });
+      const result = exec(`${makeOsSafe(executable)} --version`, { silent: true });
       this.logger.log(chalk.dim((result.stdout ?? result.stderr).replace(/\n*$/, '')));
       results[cli] = result.code === 0;
     }
