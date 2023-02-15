6b1b8fff24fbfb15d60b0df7564e2a4a7ddaedbd
mshanemc <shane.mclaughlin@salesforce.com>
2023-02-15 15:51:52 -0600

    feat: check status of gh WI

---
+++ messages/github.check.closed.md
@@ -0,0 +1,23 @@
+# summary
+
+Show open Github issues with GUS WI
+
+# description
+
+Description of a command.
+
+# flags.name.summary
+
+Description of a flag.
+
+# examples
+
+- <%= config.bin %> <%= command.id %> -o me@gus.com
+
+# flags.gus
+
+Username/alias of your GUS org connection
+
+# flags.github-token
+
+Github token--store this in the environment as GITHUB_TOKEN
---
+++ src/commands/github/check/closed.ts
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2023, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+import { SfCommand, Flags } from '@salesforce/sf-plugins-core';
+import { Messages } from '@salesforce/core';
+import { Octokit } from '@octokit/core';
+import { throttling } from '@octokit/plugin-throttling';
+import { isObject } from '@salesforce/ts-types';
+
+Messages.importMessagesDirectory(__dirname);
+const messages = Messages.loadMessages('@salesforce/plugin-release-management', 'github.check.closed');
+
+export type GithubCheckClosedResult = {
+  issueUrl: string;
+  status: string;
+  workItem: string;
+};
+
+export default class GithubCheckClosed extends SfCommand<GithubCheckClosedResult[]> {
+  public static readonly summary = messages.getMessage('summary');
+  public static readonly description = messages.getMessage('description');
+  public static readonly examples = messages.getMessages('examples');
+
+  public static readonly flags = {
+    gus: Flags.requiredOrg({
+      summary: messages.getMessage('flags.gus'),
+    }),
+    'github-token': Flags.string({
+      summary: messages.getMessage('flags.github-token'),
+      env: 'GITHUB_TOKEN',
+      required: true,
+    }),
+  };
+
+  public async run(): Promise<GithubCheckClosedResult[]> {
+    const { flags } = await this.parse(GithubCheckClosed);
+    const ThrottledOctokit = Octokit.plugin(throttling);
+    const octokit = new ThrottledOctokit({
+      auth: flags['github-token'],
+      throttle: {
+        onRateLimit: () => true,
+        onSecondaryRateLimit: () => true,
+      },
+    });
+
+    const issues = await octokit.request('GET /search/issues', {
+      q: 'is:open is:issue repo:forcedotcom/cli W- in:comments',
+    });
+
+    const commentsWithWI = (
+      await Promise.all(
+        issues.data.items.map((issue) =>
+          octokit.request('GET /repos/{owner}/{repo}/issues/{issue_number}/comments', {
+            // eslint-disable-next-line camelcase
+            issue_number: issue.number,
+            owner: 'forcedotcom',
+            repo: 'cli',
+          })
+        )
+      )
+    )
+      .map((issueComments) => issueComments.data.find((comment) => comment.body.includes('W-')))
+      .filter(isObject)
+      .map((comment) => ({ issueUrl: comment.issue_url, workItem: comment.body.match(/W-[0-9]{8,9}/g) }))
+      .filter((item) => item.workItem?.length)
+      .map((item) => ({ issueUrl: item.issueUrl, workItem: item.workItem[0] }));
+
+    const wiToQuery = commentsWithWI.map((item) => item.workItem);
+    const wiQueryResult = new Map<string, string>(
+      (
+        await flags.gus
+          .getConnection()
+          .sobject('ADM_Work__c')
+          // eslint-disable-next-line camelcase
+          // .find({ Name: { $in: wiToQuery }, Status__c: { $like: 'Closed%' } })
+          .find({ Name: { $in: wiToQuery } })
+      ).map((item) => [item.Name, item.Status__c])
+    );
+
+    const results = commentsWithWI
+      .map(
+        (item): GithubCheckClosedResult => ({
+          ...item,
+          status: wiQueryResult.get(item.workItem),
+          issueUrl: item.issueUrl.replace('api.', '').replace('repos/', ''),
+        })
+      )
+      .filter((item) => item.status);
+    this.table(
+      results,
+      {
+        issueUrl: { header: 'github issue' },
+        workItem: { header: 'work item' },
+        status: { header: 'status' },
+      },
+      { sort: 'status' }
+    );
+    return results;
+  }
+}
