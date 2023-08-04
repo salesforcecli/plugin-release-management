35022f9d7eb4ec8cbac1879cd080572e4e85ab31
mshanemc <shane.mclaughlin@salesforce.com>
2023-08-04 11:48:32 -0500

    feat: remove more files

--- src/commands/cli/tarballs/prepare.ts
+++ src/commands/cli/tarballs/prepare.ts
@@ -5,8 +5,9 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
+import { rm } from 'node:fs/promises';
 import * as fg from 'fast-glob';
-import { pwd, rm } from 'shelljs';
+import { pwd } from 'shelljs';
 import { Flags, SfCommand } from '@salesforce/sf-plugins-core';
 import { Messages } from '@salesforce/core';
 import { red } from 'chalk';
@@ -50,33 +51,51 @@ export default class Prepare extends SfCommand<void> {
 
     // Remove JSforceTestSuite from dist
     const jsForceTestSuite = await find(`${baseDirGlob}/JSforceTestSuite`, { onlyDirectories: true });
-    this.remove(jsForceTestSuite, 'JSforceTestSuite files');
+    await this.remove(jsForceTestSuite, 'JSforceTestSuite files');
+    await this.remove(await find(`${baseDirGlob}/jsforce/src`), 'jsforce src');
+    await this.remove(await find(`${baseDirGlob}/jsforce/browser`), 'jsforce browser');
+
+    // salesforce plugin schemas
+    await this.remove(await find(`${baseDirGlob}/@salesforce/plugin-*/schemas`), 'plugin schema files');
+
+    // isogit cleanup
+    await this.remove(await find(`${baseDirGlob}/**/.size_report.html`), 'isogit size results');
+
+    // minified js
+    await this.remove(await find(`${baseDirGlob}/**/*.min.js`), 'minified js');
+    await this.remove(await find(`${baseDirGlob}/rxjs/src`), 'rxjs src');
+    await this.remove(await find(`${baseDirGlob}/**/ajv/lib`), 'ajv lib');
+    await this.remove(await find(`${baseDirGlob}/**/jszip/lib`), 'jszip lib');
 
     // Module readmes and other markdown docs not found in template directories
-    const markdownFiles = await find(`${baseDirGlob}/**/*.md`, {
+    const markdownFiles = await find(`${baseDirGlob}/**/*.+(md|markdown)`, {
       excludeDirectories: ['templates', 'messages'],
     });
-    this.remove(markdownFiles, '.md files');
+    await this.remove(markdownFiles, '.md files');
 
     // Module .gitignore not found in template directories
     const gitignore = await find(`${baseDirGlob}/**/.gitignore`, { excludeDirectories: ['templates'] });
-    this.remove(gitignore, '.gitignore files');
+    await this.remove(gitignore, '.gitignore files');
 
     // Module .gitattributes not found in template directories
     const gitattributes = await find(`${baseDirGlob}/**/.gitattributes`, { excludeDirectories: ['templates'] });
-    this.remove(gitattributes, '.gitattributes files');
+    await this.remove(gitattributes, '.gitattributes files');
 
     // Module .eslintrc not found in template directories
     const eslintrc = await find(`${baseDirGlob}/**/.eslintrc`, { excludeDirectories: ['templates'] });
-    this.remove(eslintrc, '.eslintrc files');
+    await this.remove(eslintrc, '.eslintrc files');
 
     // Module appveyor.yml not found in template directories
     const appveyor = await find(`${baseDirGlob}/**/appveyor.yml`, { excludeDirectories: ['templates'] });
-    this.remove(appveyor, 'appveyor.yml files');
+    await this.remove(appveyor, 'appveyor.yml files');
 
     // Module circle.yml not found in template directories
     const circle = await find(`${baseDirGlob}/**/circle.yml`, { excludeDirectories: ['templates'] });
-    this.remove(circle, 'circle.yml files');
+    await this.remove(circle, 'circle.yml files');
+
+    // svg graphs
+    const graphs = await find(`${baseDirGlob}/**/graph.svg`);
+    await this.remove(graphs, 'svg graphs');
 
     // Module test dirs, except in salesforce-alm, which includes production source code in such a dir
     const allowedTestDirs = [
@@ -91,31 +110,31 @@ export default class Prepare extends SfCommand<void> {
     const testDirs = (await find(`${baseDirGlob}/**/test`, { onlyDirectories: true })).filter(
       (f) => !allowedTestDirs.some((d) => f.includes(d))
     );
-    this.remove(testDirs, 'test directories');
+    await this.remove(testDirs, 'test directories');
 
     // JS map files, except the ill-named `lodash.map` (it's a directory and we'll also filter out matches if found for good measure)
     const maps = (await find(`${baseDirGlob}/**/*.map`)).filter((f) => !f.includes('lodash.map'));
-    this.remove(maps, '*.map files');
+    await this.remove(maps, '*.map files');
 
     // In case yarn autoclean is disabled, delete some known windows file path length offenders
     const nycOutput = await find(`${baseDirGlob}/**/.nyc_output`);
-    this.remove(nycOutput, '.nyc_output files');
+    await this.remove(nycOutput, '.nyc_output files');
 
     // Large files shipped with jsforce
     const jsforceBuild = await find(`${baseDirGlob}/jsforce/build`, { onlyDirectories: true });
-    this.remove(jsforceBuild, 'jsforce/build directory');
+    await this.remove(jsforceBuild, 'jsforce/build directory');
 
     // This breaks compilation. We need to probably do this right before the pack, but then this will
     // break compilation the next time compile is ran without doing a yarn install --force
     // We don't need types in the production code
     if (this.flags.types) {
       const types = await find(`${baseDirGlob}/**/*.d.ts`);
-      this.remove(types, '*.d.ts files');
+      await this.remove(types, '*.d.ts files');
     }
   }
 
-  private remove(files: string[], type: string): void {
-    if (!files.length) return;
+  private async remove(files: string[], type: string): Promise<void[]> {
+    if (!files.length) return [];
     const msg = this.flags.dryrun
       ? `${red.bold('[DRYRUN] Removing:')} ${files.length} ${type}`
       : `${red.bold('Removing:')} ${files.length} ${type}`;
@@ -125,9 +144,7 @@ export default class Prepare extends SfCommand<void> {
       files.forEach((f) => this.log(`  ${f}`));
     }
 
-    if (!this.flags.dryrun) {
-      files.forEach((f) => rm('-rf', f));
-    }
+    return Promise.all(files.map((f) => (this.flags.dryrun ? undefined : rm(f, { recursive: true, force: true }))));
   }
 }
 
