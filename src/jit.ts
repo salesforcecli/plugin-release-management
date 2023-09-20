d59ebcc027aea0bafa5ad791ab0c0ecfe32c1076
Mike Donnalley <mdonnalley@salesforce.com>
2023-09-20 09:11:35 -0600

    feat: parallelize jit tests

--- src/jit.ts
+++ src/jit.ts
@@ -9,13 +9,12 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as os from 'os';
 import { promisify } from 'util';
-import { exec as execSync, ExecException } from 'child_process';
+import { exec as execSync } from 'child_process';
 import { Ux } from '@salesforce/sf-plugins-core';
 import * as chalk from 'chalk';
 import { SfError } from '@salesforce/core';
 import { parseJson } from '@salesforce/kit';
 import { Interfaces } from '@oclif/core';
-import stripAnsi = require('strip-ansi');
 import { PackageJson } from './package';
 
 const exec = promisify(execSync);
@@ -35,22 +34,7 @@ export async function testJITInstall(options: Options): Promise<void> {
   await fs.promises.rm(tmpDir, { recursive: true, force: true });
   await fs.promises.mkdir(tmpDir, { recursive: true });
 
-  const dataDir = path.join(tmpDir, 'data');
-  const cacheDir = path.join(tmpDir, 'cache');
-  const configDir = path.join(tmpDir, 'config');
-
-  process.env.SF_DATA_DIR = dataDir;
-  process.env.SF_CACHE_DIR = cacheDir;
-  process.env.SF_CONFIG_DIR = configDir;
-
-  await fs.promises.mkdir(dataDir, { recursive: true });
-  await fs.promises.mkdir(cacheDir, { recursive: true });
-  await fs.promises.mkdir(configDir, { recursive: true });
-
   ux.styledHeader('Testing JIT installation');
-  ux.log(`SF_DATA_DIR: ${dataDir}`);
-  ux.log(`SF_CACHE_DIR: ${cacheDir}`);
-  ux.log(`SF_CONFIG_DIR: ${configDir}`);
 
   const fileData = await fs.promises.readFile('package.json', 'utf8');
   const packageJson = parseJson(fileData) as PackageJson;
@@ -81,7 +65,7 @@ export async function testJITInstall(options: Options): Promise<void> {
     }
   };
 
-  const verifyInstall = async (plugin: string): Promise<boolean> => {
+  const verifyInstall = async (plugin: string, dataDir: string): Promise<boolean> => {
     const userPjsonRaw = await fs.promises.readFile(path.join(dataDir, 'package.json'), 'utf-8');
 
     const userPjson = parseJson(userPjsonRaw) as PackageJson;
@@ -90,50 +74,52 @@ export async function testJITInstall(options: Options): Promise<void> {
 
   const passedInstalls: string[] = [];
   const failedInstalls: string[] = [];
-  // We have to test these serially in order to avoid issues when running plugin installs concurrently.
-  for (const plugin of jitPlugins) {
-    try {
+
+  await Promise.all(
+    jitPlugins.map(async (plugin) => {
       ux.log(`Testing JIT install for ${plugin}`);
-      const firstCommand = commands.find((c) => c.pluginName === plugin);
-      if (!firstCommand) {
-        throw new SfError(`Unable to find command for ${plugin}`);
-      }
+      const dataDir = path.join(tmpDir, plugin, 'data');
+      const cacheDir = path.join(tmpDir, plugin, 'cache');
+      const configDir = path.join(tmpDir, plugin, 'config');
 
-      // Test that --help works on JIT commands
-      const helpResult = await help(firstCommand.id);
-      ux.log(`${executable} ${firstCommand.id} --help ${helpResult ? chalk.green('PASSED') : chalk.red('FAILED')}`);
+      await fs.promises.mkdir(dataDir, { recursive: true });
+      await fs.promises.mkdir(cacheDir, { recursive: true });
+      await fs.promises.mkdir(configDir, { recursive: true });
 
-      ux.log(`${executable} ${firstCommand.id}`);
-      // Test that executing the command will trigger JIT install
-      // This will likely always fail because we're not providing all the required flags or it depends on some other setup.
-      // However, this is okay because all we need to verify is that running the command will trigger the JIT install
-      const { stdout, stderr } = await exec(`${executable} ${firstCommand.id}`);
-      ux.log(stripAnsi(stdout));
-      ux.log(stripAnsi(stderr));
-    } catch (e) {
-      const err = e as ExecException;
-      // @ts-expect-error ExecException type doesn't have a stdout or stderr property
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-      ux.log(stripAnsi(err.stdout));
-      // @ts-expect-error ExecException type doesn't have a stdout or stderr property
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-      ux.log(stripAnsi(err.stderr));
-    } finally {
-      const result = await verifyInstall(plugin);
-      if (result) {
-        ux.log(`✅ ${chalk.green(`Verified installation of ${plugin}\n`)}`);
-        passedInstalls.push(plugin);
-      } else {
-        ux.log(`❌ ${chalk.red(`Failed installation of ${plugin}\n`)}`);
-        failedInstalls.push(plugin);
+      try {
+        const firstCommand = commands.find((c) => c.pluginName === plugin);
+        if (!firstCommand) {
+          throw new SfError(`Unable to find command for ${plugin}`);
+        }
+
+        // Test that --help works on JIT commands
+        const helpResult = await help(firstCommand.id);
+        ux.log(`${executable} ${firstCommand.id} --help ${helpResult ? chalk.green('PASSED') : chalk.red('FAILED')}`);
+
+        ux.log(`${executable} ${firstCommand.id}`);
+        // Test that executing the command will trigger JIT install
+        // This will likely always fail because we're not providing all the required flags or it depends on some other setup.
+        // However, this is okay because all we need to verify is that running the command will trigger the JIT install
+        await exec(`${executable} ${firstCommand.id}`, {
+          env: {
+            ...process.env,
+            SF_DATA_DIR: dataDir,
+            SF_CACHE_DIR: cacheDir,
+            SF_CONFIG_DIR: configDir,
+          },
+        });
+      } finally {
+        const result = await verifyInstall(plugin, dataDir);
+        if (result) {
+          ux.log(`✅ ${chalk.green(`Verified installation of ${plugin}\n`)}`);
+          passedInstalls.push(plugin);
+        } else {
+          ux.log(`❌ ${chalk.red(`Failed installation of ${plugin}\n`)}`);
+          failedInstalls.push(plugin);
+        }
       }
-      // Move the data dir so that we can start with a clean slate for the next plugin
-      // Deleting the dir would waste time, especially on Windows
-      // These prevents the 'oclif.lock' install refresh from slowing down the test
-      // There are integration tests in '@oclif/plugin-plugins' that test the oclif.lock refresh
-      await fs.promises.rename(dataDir, path.join(tmpDir, `data-${plugin.replace('@salesforce/', '')}`));
-    }
-  }
+    })
+  );
 
   ux.styledHeader('JIT Installation Results');
   ux.log(`Passed (${passedInstalls.length})`);
