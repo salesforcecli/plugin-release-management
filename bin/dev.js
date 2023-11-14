8b1bec9b253e52d4500c3d2925a756b6d306d69e
Mike Donnalley <mdonnalley@salesforce.com>
2023-11-14 15:32:52 -0700

    fix: use __dirname

--- bin/dev.js
+++ bin/dev.js
@@ -2,7 +2,7 @@
 // eslint-disable-next-line node/shebang
 async function main() {
   const { execute } = await import('@oclif/core');
-  await execute({ development: true, dir: import.meta.url });
+  await execute({ development: true, dir: __dirname });
 }
 
 await main();
--- bin/run.js
+++ bin/run.js
@@ -3,7 +3,7 @@
 // eslint-disable-next-line node/shebang
 async function main() {
   const { execute } = await import('@oclif/core');
-  await execute({ dir: import.meta.url });
+  await execute({ dir: __dirname });
 }
 
 await main();
