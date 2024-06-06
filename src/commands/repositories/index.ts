117285c991286cf2ee29a58c14a0a256e3e60ea8
Mike Donnalley <mdonnalley@salesforce.com>
2024-06-06 12:01:49 -0600

    feat: use oclif/core v4

--- src/commands/repositories/index.ts
+++ src/commands/repositories/index.ts
@@ -5,9 +5,10 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import { SfCommand } from '@salesforce/sf-plugins-core';
+/* eslint-disable sf-plugin/no-hardcoded-messages-flags */
+
+import { Flags, SfCommand, Ux } from '@salesforce/sf-plugins-core';
 import { Messages } from '@salesforce/core';
-import { ux } from '@oclif/core';
 import { RepositoryInfo, retrieveKnownRepositories } from '../../repositories.js';
 
 Messages.importMessagesDirectoryFromMetaUrl(import.meta.url);
@@ -19,35 +20,64 @@ export default class Repositories extends SfCommand<RepositoryResult> {
   public static readonly description = messages.getMessage('description');
   public static readonly examples = messages.getMessages('examples');
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   public static readonly flags = {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    ...(ux.table.flags() as any),
+    columns: Flags.string({
+      summary: messages.getMessage('flags.columns.summary'),
+      exclusive: ['extended'],
+    }),
+    csv: Flags.boolean({
+      summary: messages.getMessage('flags.csv.summary'),
+      exclusive: ['no-truncate'],
+    }),
+    extended: Flags.boolean({
+      char: 'x',
+      summary: messages.getMessage('flags.extended.summary'),
+      exclusive: ['columns'],
+    }),
+    filter: Flags.string({
+      summary: messages.getMessage('flags.filter.summary'),
+    }),
+    'no-header': Flags.boolean({
+      summary: messages.getMessage('flags.no-header.summary'),
+      exclusive: ['csv'],
+    }),
+    'no-truncate': Flags.boolean({
+      summary: messages.getMessage('flags.no-truncate.summary'),
+      exclusive: ['csv'],
+    }),
+    output: Flags.string({
+      summary: messages.getMessage('flags.output.summary'),
+      exclusive: ['no-truncate', 'csv'],
+      options: ['csv', 'json', 'yaml'],
+    }),
+    sort: Flags.string({
+      summary: messages.getMessage('flags.sort.summary'),
+    }),
   };
 
   public async run(): Promise<RepositoryResult> {
     const { flags } = await this.parse(Repositories);
     const repositories = await retrieveKnownRepositories();
+    const ux = new Ux({ jsonEnabled: flags.json ?? false });
 
-    if (!flags.json) {
-      ux.table(
-        repositories,
-        {
-          organization: {},
-          name: {},
-          url: {},
-          packages: {
-            get: (row: RepositoryInfo): string => row.packages.map((pkg) => `${pkg.type} ${pkg.name}`).join('\n'),
-            extended: true,
-          },
+    ux.table(
+      repositories,
+      {
+        organization: {},
+        name: {},
+        url: {},
+        packages: {
+          get: (row: RepositoryInfo): string => row.packages.map((pkg) => `${pkg.type} ${pkg.name}`).join('\n'),
+          extended: true,
         },
-        {
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-          printLine: this.log.bind(this),
-          ...flags, // parsed flags
-        }
-      );
-    }
+      },
+      {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        printLine: this.log.bind(this),
+        ...flags, // parsed flags
+      }
+    );
+
     return repositories;
   }
 }
